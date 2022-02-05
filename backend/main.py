173ec13ba415fb300520bee0955f2df67ab7cb6f
Maggie Huang <maggiehua01@gmail.com>
2022-02-05 18:24:54 -0500

    <feat> finished backend api

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+
+from typing import List
+from Arxiv import Arxiv
+from Document import Document
+from textsimilarity import text_similarity
 
 
 app = FastAPI()
@@ -6,4 +11,12 @@ app = FastAPI()
 
 @app.get('/')
 async def root():
-    return {"message": "Hello World!"}
+    arxiv = Arxiv()
+    docs = text_similarity(arxiv.query(['covid', 'pandemic'], 10))
+    return docs
+
+@app.get('/research/')
+async def research(max: int, key: List[str] = Query(..., min_length=1)):
+    arxiv = Arxiv()
+    docs = text_similarity(arxiv.query(key, max))
+    return docs
